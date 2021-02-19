3f581a1b4c0570f2f495ed07f1443bf2c04a95bc
Sanatdey <41385452+Sanatdey@users.noreply.github.com>
2021-02-19 16:39:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,35 +5,15 @@ const WebSocket = require('ws');
 // const server = require('http').createServer(app);
 // const wss = new WebSocket.Server({ server });
 const wss = new WebSocket.Server({
-    port: port,
-    perMessageDeflate: {
-      zlibDeflateOptions: {
-        // See zlib defaults.
-        chunkSize: 1024,
-        memLevel: 7,
-        level: 3
-      },
-      zlibInflateOptions: {
-        chunkSize: 10 * 1024
-      },
-      // Other options settable:
-      clientNoContextTakeover: true, // Defaults to negotiated value.
-      serverNoContextTakeover: true, // Defaults to negotiated value.
-      serverMaxWindowBits: 10, // Defaults to negotiated value.
-      // Below options specified as default values.
-      concurrencyLimit: 10, // Limits zlib concurrency for perf.
-      threshold: 1024 // Size (in bytes) below which messages
-      // should not be compressed.
-    }
+    port: port
   });
 // console.log(server);
 wss.on('connection', function connection(ws) {
     console.log('New Client Joined');
-    ws.send('Client Connected');
     ws.on('message', function incoming(message) {
       console.log('received:');
       console.log(message);
-      ws.send('Get Your Msg');
+      // ws.send('Get Your Msg');
       wss.clients.forEach(function each(client) {
         if (client.readyState === WebSocket.OPEN) {
           client.send(message);
